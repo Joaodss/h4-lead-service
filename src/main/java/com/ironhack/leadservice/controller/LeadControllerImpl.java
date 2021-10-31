9152a14ebd8e814dc19aaaecff92452dfc3e55bb
João Afonso Silva <joaoafonsodss@gmail.com>
2021-10-31 19:12:11 +0000

    Update leads path

--- src/main/java/com/ironhack/leadservice/controller/LeadControllerImpl.java
+++ src/main/java/com/ironhack/leadservice/controller/LeadControllerImpl.java
@@ -14,7 +14,7 @@ import java.util.NoSuchElementException;
 import static org.springframework.http.HttpStatus.*;
 
 @RestController
-@RequestMapping("/lead")
+@RequestMapping("/leads")
 public class LeadControllerImpl implements LeadController {
   private final LeadService leadService;
   private final ReportService reportService;
